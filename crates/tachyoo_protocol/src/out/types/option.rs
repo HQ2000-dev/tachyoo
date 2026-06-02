6cfbbc94f63ae2d87982e71dff520aeccfadc9d3
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-03 00:17:19 +0200

    wip Optional impl (out)

--- crates/tachyoo_protocol/src/out/types/option.rs
+++ crates/tachyoo_protocol/src/out/types/option.rs
@@ -1,34 +1,79 @@
-use tokio::io;
+pub mod unprefixed {
+    use tokio::io;
 
-use crate::out::{IntoTransferable, Transfer};
+    use crate::out::{IntoTransferable, Transfer};
 
-#[derive()]
-pub struct Optional<T>(Option<T>);
+    #[derive()]
+    pub struct Optional<T>(Option<T>);
 
-impl<T> IntoTransferable for Option<T>
-where
-    T: IntoTransferable,
-    <T as IntoTransferable>::Transferable: Send + Sync,
-{
-    type Transferable = Optional<<T as IntoTransferable>::Transferable>;
-    type Error = <T as IntoTransferable>::Error;
+    impl<T> IntoTransferable for Option<T>
+    where
+        T: IntoTransferable,
+        <T as IntoTransferable>::Transferable: Send + Sync,
+    {
+        type Transferable = Optional<<T as IntoTransferable>::Transferable>;
+        type Error = <T as IntoTransferable>::Error;
 
-    fn try_into_transferable(self) -> Result<Self::Transferable, Self::Error> {
-        Ok(Optional(
-            self.map(|t| t.try_into_transferable()).transpose()?,
-        ))
+        fn try_into_transferable(self) -> Result<Self::Transferable, Self::Error> {
+            Ok(Optional(
+                self.map(|t| t.try_into_transferable()).transpose()?,
+            ))
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl<T> Transfer for Optional<T>
+    where
+        T: Transfer + Send + Sync,
+    {
+        async fn write_to_tcp_stream(
+            &self,
+            stream: tokio::net::TcpStream,
+        ) -> Result<(), io::Error> {
+            match self.0 {
+                Some(ref t) => t.write_to_tcp_stream(stream).await,
+                None => Ok(()),
+            }
+        }
     }
 }
 
-#[async_trait::async_trait]
-impl<T> Transfer for Optional<T>
-where
-    T: Transfer + Send + Sync,
-{
-    async fn write_to_tcp_stream(&self, stream: tokio::net::TcpStream) -> Result<(), io::Error> {
-        match self.0 {
-            Some(ref t) => t.write_to_tcp_stream(stream).await,
-            None => Ok(()),
+pub mod prefixed {
+    use tokio::io;
+
+    use crate::out::{IntoTransferable, Transfer};
+
+    #[derive()]
+    pub struct PrefixedOptional<T>(Option<T>);
+
+    impl<T> IntoTransferable for PrefixedOptional<T>
+    where
+        T: IntoTransferable,
+        <T as IntoTransferable>::Transferable: Send + Sync,
+    {
+        type Transferable = PrefixedOptional<<T as IntoTransferable>::Transferable>;
+        type Error = <T as IntoTransferable>::Error;
+
+        fn try_into_transferable(self) -> Result<Self::Transferable, Self::Error> {
+            Ok(PrefixedOptional(
+                self.map(|t| t.try_into_transferable()).transpose()?,
+            ))
+        }
+    }
+
+    #[async_trait::async_trait]
+    impl<T> Transfer for PrefixedOptional<T>
+    where
+        T: Transfer + Send + Sync,
+    {
+        async fn write_to_tcp_stream(
+            &self,
+            stream: tokio::net::TcpStream,
+        ) -> Result<(), io::Error> {
+            match self.0 {
+                Some(ref t) => t.write_to_tcp_stream(stream).await,
+                None => Ok(()),
+            }
         }
     }
 }
