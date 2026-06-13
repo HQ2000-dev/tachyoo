84e25ed39ce9cd412457bfba02da5bab7de1e8ce
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-14 01:27:11 +0200

    extend protocol parser stub

--- crates/tachyoo_protocol/src/in_/mod.rs
+++ crates/tachyoo_protocol/src/in_/mod.rs
@@ -1,18 +1,35 @@
-pub mod state;
+pub mod packet_ids;
 pub mod types;
 pub mod var;
 
-use bytes::{BufMut, BytesMut};
-use tokio::io::{self, AsyncReadExt};
+use std::io;
 
+use bytes::{BufMut, BytesMut};
+use tokio::io::AsyncReadExt;
+
+use crate::{in_::packet_ids::Packet, state::ProtocolStage};
+
+//TODO: try out making it generic over the stage
 pub struct ProtocolParser {
     buffer: BytesMut,
+    stage: ProtocolStage,
 }
 
 impl ProtocolParser {
-    /*pub async fn parse_next<R: AsyncReadExt + Unpin>(&mut self, r: &mut R) -> io::Result<Option> {
-        // r.read(buf)
+    pub fn new() -> ProtocolParser {
+        ProtocolParser {
+            buffer: BytesMut::new(),
+            stage: ProtocolStage::default(),
+        }
+    }
 
-        Ok(())
-    }*/
+    pub fn set_stage(&mut self, new_stage: ProtocolStage) {
+        self.stage = new_stage;
+    }
+
+    pub async fn parse_next<R: AsyncReadExt>(&mut self, reader: &mut R) -> io::Result<Packet> {
+        match self.stage {
+            ProtocolStage::Handshake => {}
+        }
+    }
 }
