e3e6bb6d95b61a352f132d73364b687ade10a7f6
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-14 01:28:17 +0200

    add more wip VarInt etc parsing impls (another yay in the future)

--- crates/tachyoo_protocol/src/in_/types/var.rs
+++ crates/tachyoo_protocol/src/in_/types/var.rs
@@ -1,3 +1,85 @@
 pub mod int {
-    //pub async fn parse<R: AsyncReadExt + Unpin>()
+    pub mod signed {
+    use std::io;
+
+    use leb128::read::Error;
+    use tokio::io::AsyncReadExt;
+    use tokio_util::io::SyncIoBridge;
+
+use crate::in_::types::Int;
+
+    //TODO: own implementaion!!!
+    // (this is very inefficient, I think)
+    pub async fn parse<R: AsyncReadExt + Unpin>(reader: &mut R) -> io::Result<Int> {
+        let mut bridge = SyncIoBridge::new(reader);
+        leb128::read::signed(&mut bridge)
+            .map_err(|e| match e {
+                Error::IoError(err) => err,
+                Error::Overflow => todo!(),
+            })
+            .map(|val| val as i32)
+    }
+    }
+    pub mod unsigned {
+    use std::io;
+
+    use leb128::read::Error;
+    use tokio::io::AsyncReadExt;
+    use tokio_util::io::SyncIoBridge;
+
+use crate::in_::types::UInt;
+
+    //TODO: own implementaion!!!
+    // (this is very inefficient, I think)
+    pub async fn parse<R: AsyncReadExt + Unpin>(reader: &mut R) -> io::Result<UInt> {
+        let mut bridge = SyncIoBridge::new(reader);
+        leb128::read::unsigned(&mut bridge)
+            .map_err(|e| match e {
+                Error::IoError(err) => err,
+                Error::Overflow => todo!(),
+            })
+            .map(|val| val as u32)
+    }
+    }
+}
+
+pub mod long {
+    use std::io;
+
+    use leb128::read::Error;
+    use tokio::io::AsyncReadExt;
+    use tokio_util::io::SyncIoBridge;
+
+use crate::in_::types::Long;
+
+    //TODO: own implementaion!!!
+    // (this is very inefficient, I think)
+    pub async fn parse<R: AsyncReadExt + Unpin>(reader: &mut R) -> io::Result<Long> {
+        let mut bridge = SyncIoBridge::new(reader);
+        leb128::read::signed(&mut bridge)
+            .map_err(|e| match e {
+                Error::IoError(err) => err,
+                Error::Overflow => todo!(),
+            })
+    }
+    pub mod unsigned {
+    use std::io;
+
+    use leb128::read::Error;
+    use tokio::io::AsyncReadExt;
+    use tokio_util::io::SyncIoBridge;
+
+use crate::in_::types::ULong;
+
+    //TODO: own implementaion!!!
+    // (this is very inefficient, I think)
+    pub async fn parse<R: AsyncReadExt + Unpin>(reader: &mut R) -> io::Result<ULong> {
+        let mut bridge = SyncIoBridge::new(reader);
+        leb128::read::unsigned(&mut bridge)
+            .map_err(|e| match e {
+                Error::IoError(err) => err,
+                Error::Overflow => todo!(),
+            })
+    }
+    }
 }
