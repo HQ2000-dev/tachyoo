6c4bfbba4b68870aec5ba678f70f4c688c9c30e5
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-14 21:16:40 +0200

    add missing modules

--- crates/tachyoo_protocol/src/in_/types/mod.rs
+++ crates/tachyoo_protocol/src/in_/types/mod.rs
@@ -1,10 +1,11 @@
-pub mod var;
 pub mod server_addr;
+pub mod string;
+pub mod var;
 
 pub type Long = i64;
 
 pub type Int = i32;
 
-pub type ULong=u64;
+pub type ULong = u64;
 
-pub type UInt=u32;
+pub type UInt = u32;
