30aee8c5b5b4c7ccdc7f73afe2b804ab75ae034b
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-14 21:16:16 +0200

    change `shutdown_able` to `cancel_able` (via cancellation token)

--- crates/tachyoo/src/util.rs
+++ crates/tachyoo/src/util.rs
@@ -1,14 +1,12 @@
 use std::convert::Infallible;
 
-use tokio::{select, sync::broadcast};
+use tokio::{select, sync::watch};
+use tokio_util::sync::CancellationToken;
 
 use crate::ShutdownMsg;
 
-/// a "shutdownable future" (_ -> Result<!, E>) that returns Ok(()) on receiving a shutdown message (_ -> Result<(), E>)
-pub async fn shutdown_able<F, E>(
-    mut shutdown_rx: broadcast::Receiver<ShutdownMsg>,
-    fut: F,
-) -> Result<(), E>
+/// a "cancel-able future" (_ -> Result<!, E>) that returns Ok(()) on receiving a cancellation message (_ -> Result<(), E>)
+pub async fn cancel_able<F, E>(cancel_token: CancellationToken, fut: F) -> Result<(), E>
 where
     F: Future<Output = Result<Infallible, E>>,
 {
@@ -19,7 +17,8 @@ where
                 Err(e) => Err(e),
             }
         },
-        Ok(_) = shutdown_rx.recv() => {
+        //TODO: really working??
+        _ = cancel_token.cancelled() => {
             return Ok(());
         }
     }
