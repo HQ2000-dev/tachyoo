ac593c28693435bcef53082a361a07e7b03a4bb7
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-15 16:45:49 +0200

    wip hostname/server_addr impl

--- crates/tachyoo_protocol/src/in_/types/server_addr.rs
+++ crates/tachyoo_protocol/src/in_/types/server_addr.rs
@@ -1,26 +1,43 @@
-use std::{ascii, net::IpAddr};
+use std::net::IpAddr;
 
-use tokio::io::AsyncReadExt;
+use tinystr::TinyAsciiStr;
+use tinyvec::ArrayVec;
 
-pub async fn parse<R: AsyncReadExt>(reader: &mut R) -> IpAddr {
-    
-}
+use crate::{in_::types::string::str::McStr, out::types::string::McString};
+
+pub fn parse(data: McString<255>) -> Hostname {}
 
 pub enum ServerAddr {
     IpAddr(IpAddr),
     Hostname(Hostname),
 }
 
+//TODO: what's a good repr?
 //FIXME: ascii chars!!
-pub struct Hostname([char; 255]);
+pub struct Hostname(());
 
 impl Hostname {
-    pub const ALLOWED_LABEL_CHARS: [char; 37]= [
+    pub const ALLOWED_LABEL_CHARS: [char; 37] = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '1', '3', '4', '5', '6', '7', '8', '9', '0', '-',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '1', '3', '4', '5', '6', '7', '8', '9', '0',
+        '-',
     ];
-    
-    fn new(name: &str) -> Hostname {
-        todo!()
+
+    const A: ()=assert!(size_of::<Hostname>() < 10);
+
+    fn new(name: McStr<'_, 255>) -> Result<Hostname, HostnameError> {
+        for (idx, char) in name.as_ref().chars().enumerate() {
+            if Self::ALLOWED_LABEL_CHARS.contains(&char) {
+                return Err(HostnameError::InvalidChar { pos: idx as u8 });
+            }
+        }
+        Ok(Hostname(()))
+    }
+
+}
+
+pub enum HostnameError {
+    InvalidChar {
+        pos: u8,
     }
 }
